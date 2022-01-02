79c1614ca84936e6f0c4697d460caa92f869d987
alifmuryp <alifmuryp@gmail.com>
2022-01-02 16:56:15 +0700

    fail

--- static/link.js
+++ static/link.js
@@ -1,17 +1,3 @@
-const ccls = ()=>{
-	// The URL changed...
-  const link = this.window.location.href
-  // console.log(link)
-  const cls = link.split("#")[1]
-  // console.log(cls)
-  const body =  document.getElementsByTagName("BODY")[0]
-  // console.log(body)
-if (cls==="" || cls==undefined) {
-  body.className = "home"
-}else{
-  body.className = cls
-}
-}
 ccls()
 // samakan kayak klick url
 window.addEventListener('popstate', function () {
