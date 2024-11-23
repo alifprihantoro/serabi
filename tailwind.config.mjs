b7ce329e81372d108c071051d16ebacf59585be7
alifprihantoro <alifprihantoro@gmail.com>
2024-11-23 23:12:13 +0700

    chore: update configs tailwind

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,4 +1,4 @@
-const themes = require('daisyui/src/theming/themes');
+const themes = require('daisyui/src/theming/themes')
 
 /** @type {import('tailwindcss').Config} */
 export default {
@@ -17,33 +17,14 @@ export default {
   daisyui: {
     themes: [
       {
-        dark: {
-          ...themes['dark'],
-          primary: '#b388eb',
-          secondary: '#8093f1',
-          // 'accent': '#5eead4',
-          // 'neutral': '#dededf',
-          // 'base-100': '#282A36',
-          // 'info': '#374151',
-          success: '#08605f',
-          warning: '#F1FA8C',
-          error: '#f4e04d',
-        },
         light: {
-          ...themes['corporate'],
-          primary: '#b388eb',
-          secondary: '#8093f1',
-          // 'accent': '#5eead4',
-          // 'neutral': '#dededf',
-          // 'base-100': '#282A36',
-          // 'info': '#374151',
-          success: '#08605f',
-          warning: '#F1FA8C',
-          error: '#f4e04d',
+          ...themes['cupcake'],
+          primary: '#53904e',
+          secondary: '#784b2c',
         },
       },
     ],
   },
   // eslint-disable-next-line no-undef
   plugins: [require('@tailwindcss/typography'), require('daisyui')],
-};
+}
